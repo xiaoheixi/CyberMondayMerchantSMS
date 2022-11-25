75ab08d8a4af4c719542ae5ebec15d5705e843f6
xiaoheixi <justinzhao893@gmail.com>
2022-11-25 15:19:05 +1100

    Improved fail safe for phone numbers starting with 61.

--- Program.cs
+++ Program.cs
@@ -27,11 +27,15 @@ namespace TestConsoleApp
             foreach (string x in lines)
             {
                 Message body_messages_0 = new Message();
-                body_messages_0.Content = "Dear " + x.Split(',')[2].TrimStart().Replace("'", "") + ", \nMonday 28th November is Cyber Monday. Here at YQme we are planning to give all your customers 5% off all their orders for the day when they order through YQme. This discount will be covered by YQme. We will be sending out a text to your customers, informing them of the promotion today. This promotion will help drive sales for today. If you would like to add your own promotion on top of the 5% please get in contact with us. \n \n Enjoy! \n The Team at YQme";
+                body_messages_0.Content = "Hi " + x.Split(',')[2].TrimStart().Replace("'", "") + ", \nMonday 28th November is Cyber Monday. Here at YQme we are planning to give all your customers 5% off all their orders for the day when they order through YQme. This discount will be covered by YQme. We will be sending out a text to your customers, informing them of the promotion today. This promotion will help drive sales for today. If you would like to add your own promotion on top of the 5% please get in contact with us. \n \n Enjoy! \n The Team at YQme";
                 body_messages_0.DestinationNumber = regex.Replace(x.Split(',')[4].TrimStart().Replace("'", ""), "+61", 1);
                 if (x.Split(',')[4].TrimStart().Replace("'", "").Contains("+61"))
                 {
                     body_messages_0.DestinationNumber = x.Split(',')[4].TrimStart().Replace("'", "");
+                }
+                else if (Regex.IsMatch(x.Split(',')[4].TrimStart().Replace("'", ""), "^614[0-9]{8}$"))
+                {
+                    body_messages_0.DestinationNumber = "+" + x.Split(',')[3].TrimStart().Replace("'", "");
                 }
                 else
                 {
