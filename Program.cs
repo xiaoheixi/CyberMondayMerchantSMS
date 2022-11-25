687f9a170f2a0b8b6d4c3917fb0cc4930b530464
xiaoheixi <justinzhao893@gmail.com>
2022-11-25 12:51:39 +1100

    Adjusted content of the text message.

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace TestConsoleApp
             foreach (string x in lines)
             {
                 Message body_messages_0 = new Message();
-                body_messages_0.Content = "Dear " + x.Split(',')[2].TrimStart().Replace("'", "") + ", \nMonday 28th November is Cyber Monday. Here at YQme we are planning to give all your customers 5% off all their orders for the day when they order through YQme. This discount will be covered by YQme. We will be sending out an email to your customers, informing them of the promotion this Thursday, the 24th. This will be followed up on Cyber Monday with an SMS and a link to your website. This promotion will help drive sales for a Monday. If you would like to add your own promotion on top of the 5% please get in contact with us. \n \n Enjoy! \n The Team at YQme";
+                body_messages_0.Content = "Dear " + x.Split(',')[2].TrimStart().Replace("'", "") + ", \nMonday 28th November is Cyber Monday. Here at YQme we are planning to give all your customers 5% off all their orders for the day when they order through YQme. This discount will be covered by YQme. We will be sending out a text to your customers, informing them of the promotion today. This promotion will help drive sales for today. If you would like to add your own promotion on top of the 5% please get in contact with us. \n \n Enjoy! \n The Team at YQme";
                 body_messages_0.DestinationNumber = regex.Replace(x.Split(',')[4].TrimStart().Replace("'", ""), "+61", 1);
                 if (x.Split(',')[4].TrimStart().Replace("'", "").Contains("+61"))
                 {
